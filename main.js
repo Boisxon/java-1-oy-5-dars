33cb9c005894b5da2cd43e08c77955fea3334f4a
Boisxon <boisxon357@gmail.com>
2024-12-15 11:27:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+// const userName = +prompt("Raqam kiriting")
+
+
+// if (userName % 2 == 0) {
+//     alert(`${userName} raqami juft son`)
+// }else{
+//     alert(`${userName} raqami toq son`)
+// }
+
+
+
+// const randomRaqam = Math.round(Math.random() * 100)
+
+// console.log(randomRaqam)
+
+// if (randomRaqam % 2 == 0){
+//     alert(`${randomRaqam} raqami juft son`)
+// }else{
+//     alert(`${randomRaqam} raqami toq son`)
+// }
+
+
+
+// const num1 = +prompt("birinchi raqam kiriting")
+// const num2 = +prompt("ikinchi raqam kiriting")
+
+// if (num1 > num2) {
+//     alert(`${num1} raqami ${num2} dan katta`)
+// }else if (num1 < num2){
+//     alert(`${num2} raqami ${num1} dan katta`)
+// }else if (num1 == num2){
+//     alert("ikki raqam teng")
+// }else if (null){
+//     alert(`siz raqam kiritmadingiz`)
+// }
+
+
+
+// console.log(`name:${oquvchilar.ismi} age:${oquvchilar.yoshi} uylangan:${oquvchilar.uylangammi}`);
+
+
+
+
+// ------------------- //
+
+
+// const userName = prompt("Ismingizni kiriting")
+// const userAge = +prompt("yoshingizni kiriting")
+// const userMarride = confirm("Uylangammisiz")
+
+
+// const oquvchilar = {
+//     ismi:userName,
+//     yoshi:userAge,
+//     uylangammi:userMarride,
+// }
+
+// if (oquvchilar.yoshi >= 18){
+//     console.log(`siz royhatga olindingiz` , oquvchilar);
+// }else if(userName == "" && userAge == 0){
+//     alert("Iltimos ismingizni kiriting")
+// }else {
+//     alert("Siz royhatga olinmadingiz")
+// }
+
+
+
+
+
+// ============ //
+
+
+// const num1 = +prompt("Birinchi raqamni kiriting");
+// const num2 = +prompt("Ikkinchi raqamni kiriting");
+// const anwer = +prompt("Iltimos tanlang: \n 1) + \n 2) - \n 3) * \n 4) /")
+
+// if(anwer == 1){
+//     alert(`Javob: ${num1 + num2}`)
+// }else if(anwer == 2){
+//     alert(`Javob: ${num1 - num2}`)
+// }else if(anwer == 3){
+//     alert(`Javob: ${num1 * num2}`)
+// }else if(anwer == 4){
+//     alert(`Javob: ${num1 / num2}`)
+// }else if(num1 == 0 && num2 == 0){
+//     alert("Iltimos qiymat kiriting")
+// }else if(num1 == 0){
+//     alert("Iltimos qiymat kiriting")
+// }
+// else{
+//     alert("Qiymat kiriting")
+// }
+
+
+
+const userName = prompt("Ismingizni kiriting")
+const userAge = +prompt ("id ingizni kiriting" )
+const surName = +prompt ("pasword dingizni kiriting")
+
+const infol ={
+    name:userName,
+    id:userAge,
+    pasword:surName,
+}
+
+console.log(infol);
+
+
